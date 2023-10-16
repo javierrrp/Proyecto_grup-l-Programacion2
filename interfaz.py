276ca611138d6c2e3d717e998de6bf48d92a3eb7
JavierP <javier.poblete2023@alu.uct.cl>
2023-10-16 17:39:00 -0300

    se puede ver como ordena ahora

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, ttk
 import customtkinter as ctk
-
-nRes = [1200, 800]
-
+nRes = [440, 330]
 def crear_lista():
     lista = []
     for i in range(10):
@@ -13,22 +11,28 @@ def crear_lista():
         root.destroy()
         lista.append(value)
     return lista
-
-
-
-
-
+def ordenar(lista):
+    for i in range(1, len(lista)):
+        key = lista[i]
+        j = i - 1
+        while j >= 0 and key < lista[j]:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = key
+        label.config(text=f"Lista: {lista}")
+        win.update()
+        win.after(100) 
+    return lista
 lista_creada = crear_lista()
-
 win = ctk.CTk()
 win._set_appearance_mode('light')
-win.title('Proyecto Transdiciplinario: Trabajo y Energia')
+win.title('Proyecto acumulativo')
 win.iconbitmap('Logo.ico')
 win.geometry(f'{nRes[0]}x{nRes[1]}')
 win.resizable(False, False)
 fuente = ctk.CTkFont(family='Times New Roman', size=12)
-
 label = tk.Label(win, text=f"Lista: {lista_creada}")
 label.pack()
-
+botonordena = ttk.Button(text="ordena esta lista", command=lambda: ordenar(lista_creada))
+botonordena.place(x=50, y=80)
 win.mainloop()
